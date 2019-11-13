badafbb185f9a3b082a8fa317c4d7dfc318fbb36
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-11-13 11:48:27 +0100

    191113.ai állapot

---
+++ Unity 2D Prorotype/Assets/Scripts/playerControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D myRB;
+    Animator myAnim;
+    bool facingRight;
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
